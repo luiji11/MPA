2ae02eb6d739c6ca15c9be5a66baf5c9e1b28fa3
luiji11 <jimenez.o.luis@gmail.com>
2016-04-26 19:18:04 -0700

    Update mpaInitializePars.m

--- mpaInitializePars.m
+++ mpaInitializePars.m
@@ -1,30 +1,27 @@
-function [T, R] = mpaInitializePars(expStage)
+function [T, K] = mpaInitializePars
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
 %%
-R.sampHz        = 15;                       % Sampling rate of animal's lick response
-R.pulseWidth    = 15;                       % pulse width of water valve stimuluation
+% Stuff about Sampling 
+K.sampHz        = 15;   % Sampling rate of animal's lick response
+K.pulseWidth    = 15;   % default pulse width of water valve stimuluation
+K.k             = 0;    % Start all sessions with the zeroth sample
+K.mxRewardHz    = .2;   % Max Rate of water dispense 
+
+T.sessDur       = 2 * 60;                % duration of session (in seconds)
+T.preKDuration  = 2;                     % duration of pre Recording (sampling starts before session does) 
+
+% Relevent Info
+estNumSamples   = round(K.sampHz*(T.preKDuration+T.sessDur)/10);
+I.lick          = zeros(estNumSamples, 2); 
+I.trial         = zeros(round(size(I.lick,1)/5),4); % this needs to be adjusted in the future once i figure out average animal lick rate   
+I.water         = zeros((T.sessDur*K.mxRewardHz),2);  % Frame (c1) and Port (c2) of water Reward       
+
+CMD  = {'1a',   '',     '';...
+        '2a',   '',     '';...
+        '3a',   'old',  'ord';...
+        '4',    '',     ''};        % left or right side, for triggering stimulus
 
 
-  %%  
-if strcmp( expStage, '1a') || strcmp( expStage, '2a') || strcmp( expStage, '3a') 
-    T.sessDur       = 2 * 60;                % duration of session (in seconds)
-    T.preKDuration  = 2;                     % duration of pre Recording (sampling starts before session does) 
-    
-    estNumSamples   = round(R.sampHz*(T.preKDuration+T.sessDur)/10);
-    R.sgLick        = zeros(estNumSamples, 2);            % Frame (c1) and Port (c2) of Lick
-
-    R.mxRewardHz    = 1/2;                                % Rate of water release         
-    R.sgH2O         = zeros((T.sessDur*R.mxRewardHz),2);  % Frame (c1) and Port (c2) of water Reward       
-    T.trialAnswers  = zeros(round(size(R.sgLick,1)/5),4);    
-   
-end
-
-
-if strcmp( expStage, '2a') || strcmp( expStage, '3a') 
-    R.mxRewardHz    = .5;                                       % max Rate of water release         
-    
-    T.trialChoices  = {'old', 'ord'}; % left or right side, for triggering stimulus
-end
 
